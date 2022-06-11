55bcf7426069a89ce3f4641f28be8484a7a8d52d
gabrielcannes <gabrielcannes.gc@gmail.com>
2022-06-11 12:38:58 -0300

    Facial recognition implemented

--- public/script.js
+++ public/script.js
@@ -1,10 +1,41 @@
-let canvas = document.querySelector("#canvas");
-let contect = canvas.getContext("2d");
 let video = document.querySelector("#video");
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  });
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+]).then(startVideo());
+
+function startVideo() {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    });
+  }
 }
+
+video.addEventListener("play", () => {
+  console.log("addEvent");
+  //const canvas = faceapi.createCanvasFromMedia(video.srcObject);
+  const canvas = faceapi.createCanvas(video);
+  //video.append(canvas);
+  canvas.id = "canvas";
+  document.querySelector("#video").append(canvas);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+  console.log("Event added");
+});
